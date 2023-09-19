42f72d9d502a57ecc1027bee997fdfda6a783d68
Rugaiarere <rugaiarere@gmail.com>
2023-09-18 22:59:51 -0700

    update

--- main.h
+++ main.h
@@ -4,40 +4,10 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
-
-#define BUFF_SIZE 1024
-
-/* FLAGS */
-#define F_MINUS 1
-#define F_PLUS 2
-#define F_ZERO 4
-#define F_HASH 8
-#define F_SPACE 16
-
-/* SIZES */
-#define S_LONG 2
-#define S_SHORT 1
-
-typedef struct fmt
-{
-	char fmt;
-	int (*fn)(va_list, char[], int, int, int, int);
-} fmt_t;
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
 
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i,
-va_list list, char buffer[], int flags, int width, int precision, int size);
 
-int print_char(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_string(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-int print_percent(va_list types, char buffer[],
-	int flags, int width, int precision, int size);
-
-int get_flags(const char *format, int *i);
-int get_width(const char *format, int *i, va_list list);
-int get_precision(const char *format, int *i, va_list list);
-int get_size(const char *format, int *i);
-
-#endif /* MAIN_H */
+#endif

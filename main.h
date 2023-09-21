467b0c8fa50145e67aaaffc2e2b7340df438328f
Rugaiarere <rugaiarere@gmail.com>
2023-09-21 09:13:37 -0700

    update

--- main.h
+++ main.h
@@ -15,26 +15,19 @@ int _printf(const char *format, ...);
 char character_handler(char);
 int string_handler(char *);
 int print_binary(unsigned int n);
-void print_buffer(char buffer[], int *buff_ind);
-int get_flags(const char *format, int *i);
-int get_width(const char *format, int *i, va_list list);
-int get_precision(const char *format, int *i, va_list list);
-int get_size(const char *format, int *i);
-int handle_print(const char *fmt, int *i,
-va_list list, char buffer[], int flags, int width, int precision, int size);
 
-int handle_write_char(char c, char buffer[],
-	int flags, int width, int precision, int size);
-int write_number(int is_positive, int ind, char buffer[],
-	int flags, int width, int precision, int size);
-int write_num(int ind, char bff[], int flags, int width, int precision,
-	int length, char padd, char extra_c);
-int write_pointer(char buffer[], int ind, int length,
-	int width, int flags, char padd, char extra_c, int padd_start);
 
-int write_unsgnd(int is_negative, int ind,
-char buffer[],
-	int flags, int width, int precision, int size);
-
+int print_string(const char *str);
+int print_hex(int value);
+int print_integer(int num);
+int print_unsigned(unsigned int num);
+int print_octal(unsigned int num);
+int print_unsigned_recursive(unsigned int num);
+int print_octal_recursive(unsigned int num);
+int print_custom_string(const char *s);
+int print_binary(unsigned int num);
+int print_pointer(void *ptr);
+int print_hexadecimal(unsigned int num, int uppercase);
+int print_binary_recursive(unsigned int num);
 
 #endif

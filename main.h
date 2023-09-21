e0cceb5efaab1848cd70b82dfe7df463457ed9c3
Rugaiarere <rugaiarere@gmail.com>
2023-09-21 03:53:01 -0700

    update

--- main.h
+++ main.h
@@ -8,10 +8,33 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stddef.h>
+#define BUFF_SIZE 1024
 
 int _putchar(char c);
 int _printf(const char *format, ...);
 char character_handler(char);
 int string_handler(char *);
 int print_binary(unsigned int n);
+void print_buffer(char buffer[], int *buff_ind);
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
+
+int handle_write_char(char c, char buffer[],
+	int flags, int width, int precision, int size);
+int write_number(int is_positive, int ind, char buffer[],
+	int flags, int width, int precision, int size);
+int write_num(int ind, char bff[], int flags, int width, int precision,
+	int length, char padd, char extra_c);
+int write_pointer(char buffer[], int ind, int length,
+	int width, int flags, char padd, char extra_c, int padd_start);
+
+int write_unsgnd(int is_negative, int ind,
+char buffer[],
+	int flags, int width, int precision, int size);
+
+
 #endif
